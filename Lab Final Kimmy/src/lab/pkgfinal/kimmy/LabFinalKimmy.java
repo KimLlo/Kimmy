615779057cd6f8aa8cb28f698bc5696b0a5a5096
user_name <email>
2016-11-26 11:56:44 -0600

    Sumas agregadas

---
+++ Lab Final Kimmy/src/lab/pkgfinal/kimmy/LabFinalKimmy.java
@@ -0,0 +1,19 @@
+
+package lab.pkgfinal.kimmy;
+
+
+public class LabFinalKimmy {
+
+   
+    public static void main(String[] args) {
+        
+        int n1= 1;
+        int n2= 2;
+        int Total=0;
+        
+        Total=n1+n2;
+        
+        System.out.println("El total es " + Total);
+    }
+    
+}
